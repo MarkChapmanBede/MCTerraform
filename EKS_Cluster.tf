ad6e2f88c2e8aaf34a9427f26939ac017eb5b454
Mark Chapman <41584964+MarkChapmanBede@users.noreply.github.com>
2023-11-22 16:53:08 +0000

    Update EKS_Cluster.tf

--- EKS_Cluster.tf
+++ EKS_Cluster.tf
@@ -1,9 +1,9 @@
 resource "aws_eks_cluster" "BudCluster" {
   name     = "my-eks-cluster"
-  role_arn = "arn:aws:iam::173278684542:role/eksClusterRole"
+  role_arn = var.cluster_role_arn
 
   vpc_config {
-    subnet_ids = ["subnet-0081d4e8f1f3a2c68", "subnet-0fcf489706f708d5f, subnet-09cdefedcb2756073, subnet-0d73f49f1059d0e99"]
+    subnet_ids = var.subnet_ids 
     endpoint_private_access = true
     endpoint_public_access  = true
   }
