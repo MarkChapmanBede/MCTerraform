f0281594520470eff8f9d42520b4957dfb1369ae
Mark Chapman <41584964+MarkChapmanBede@users.noreply.github.com>
2023-11-22 16:54:58 +0000

    Update AnotherNodegroup.tf

--- AnotherNodegroup.tf
+++ AnotherNodegroup.tf
@@ -1,8 +1,8 @@
 resource "aws_eks_node_group" "BudCluster" {
   cluster_name    = aws_eks_cluster.BudCluster.name
   node_group_name = "ANOTHER"
-  node_role_arn   = "arn:aws:iam::173278684542:role/AmazonEKSNodeRole"  # Replace with your IAM role ARN
-  subnet_ids      = ["subnet-0081d4e8f1f3a2c68", "subnet-0fcf489706f708d5f", "subnet-09cdefedcb2756073", "subnet-0d73f49f1059d0e99"]
+  node_role_arn   = var.node_role_arn
+  subnet_ids      = var.subnet_ids
 
   scaling_config {
     desired_size = 1
